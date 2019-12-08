69f6dc7613e876648321d62aab934defddc03ecf
Tutkovics <tutiandras@gmail.com>
2019-12-09 00:17:49 +0100

    bugfix nginx 0 qps. Add predict function to average_draw.py

--- results/average_draw.py
+++ results/average_draw.py
@@ -18,15 +18,16 @@ def plot_from_file(json_files):
 
         with open(json_file) as json_result:
             raw_data = json.load(json_result)
-            select_by_pod_number(raw_data)
+            name = json_file.split("/")[1].split("-")[0]
+            select_by_pod_number(raw_data, name)
 
 
-def select_by_pod_number(raw_data):
+def select_by_pod_number(raw_data, name):
     pod_number = raw_data["number_of_pod"]
     cpu_limit = raw_data["cpu_limit"]
     memory_limit = raw_data["memory_limit"]
 
-    unique = str(pod_number) + "pod-" + cpu_limit + "CPU-" + memory_limit
+    unique = name + ": " + str(pod_number) + "pod-" + cpu_limit + "Cpu-" + memory_limit
 
     tmp = []
     last_qps = -1
@@ -40,7 +41,7 @@ def select_by_pod_number(raw_data):
     if unique in datas:
         datas[unique].append(tmp)
     else:
-        datas[unique] = []
+        datas[unique] = [tmp] # first occurrence was miss
 
 
 def data_process_and_visualize():
@@ -52,9 +53,11 @@ def data_process_and_visualize():
             for i in range(len(measurement)):
                 if str(measurement[i][0]) in colletcion:
                     # already key presented in dictionary
+
                     colletcion[str(measurement[i][0])]["actual"].append(measurement[i][1])
                     colletcion[str(measurement[i][0])]["cpu"].append(measurement[i][2])
                     colletcion[str(measurement[i][0])]["memory"].append(measurement[i][3])
+
 
                 else:
                     colletcion[str(measurement[i][0])] = {
@@ -66,16 +69,32 @@ def data_process_and_visualize():
         x = []
         y = []
 
-        for point in colletcion:
-            x.append(statistics.mean(colletcion[point]["actual"]))
-            if str(sys.argv[1]) == "cpu":
-                y.append(statistics.mean(colletcion[point]["cpu"]))
-            elif str(sys.argv[1]) == "memory":
-                y.append(statistics.mean(colletcion[point]["memory"]))
-        #print(x)
-        #print(y)
+        last_qps = -1
 
-        plt.plot(x, y, label=key)
+        for point in colletcion:
+            if last_qps < statistics.mean(colletcion[point]["actual"]):
+                x.append(statistics.mean(colletcion[point]["actual"]))
+                if str(sys.argv[1]) == "cpu":
+                    y.append(statistics.mean(colletcion[point]["cpu"]))
+                elif str(sys.argv[1]) == "memory":
+                    y.append(statistics.mean(colletcion[point]["memory"]))
+                
+                last_qps = statistics.mean(colletcion[point]["actual"])
+
+        
+
+        if "1pod" in key:
+            predict_own_usage(colletcion,1, 3, 50, "Predict from 1 pod version")
+        elif "2pod" in key:
+            predict_own_usage(colletcion,2, 3, 50, "Predict from 2 pod version")
+        elif "3pod" in key:
+            pass
+            #predict_own_usage(colletcion,3, 3, 50, "Predict from 3 pod version")
+        elif "4pod" in key:
+            predict_own_usage(colletcion,4, 3, 50, "Predict from 4 pod version")
+        
+        if "1pod" in key: 
+            plt.plot(x, y, label=key)
 
     if(str(sys.argv[1]) == "cpu"):
         plt.ylabel("SUM CPU / sec")
@@ -90,6 +109,52 @@ def data_process_and_visualize():
 
     plt.show()
 
+def answer_y(x1, y1, x2, y2, ask_x):
+    # answer y number (cpu/memory usage) beetween two point in a given x value
+    # use line's normal method Ax + By = Ax1 + By1
+    # normalvector values (A,B):
+    A = y1 - y2
+    B = x2 - x1
+
+    y = ( (A*x1) + (B*y1) - (A*ask_x) ) / B
+    #print("Calculate: (" + str(x1) + ', ' + str(y1) + ") ("+ str(x2) +", "+ str(y2)+") " + str(ask_x)+" --> " + str(y))
+    return y
+
+def predict_own_usage(raw_data, old_pod_number, new_pod_number, granularity, plot_label):
+    # get end of qps (Q):
+    last_key = ""
+    for key in raw_data:
+        last_key = key
+    Q = (new_pod_number / old_pod_number) * (statistics.mean(raw_data[last_key]["actual"]))
+
+    # x and y arrays for points
+    x = []
+    y = []
+
+    # get data points to show
+    for ask_qps in range(0, int(Q), granularity):
+        last_point = [0,0]
+
+        # only if qps numbers in increment order
+        for key in raw_data:
+            find_qps = ask_qps * (old_pod_number / new_pod_number)
+            if (find_qps <= statistics.mean(raw_data[key]["actual"])) and (find_qps >= last_point[0]):
+                if find_qps == statistics.mean(raw_data[key]["actual"]):
+                    x.append(ask_qps)
+                    y.append(statistics.mean(raw_data[key]["cpu"]))
+                else:
+                    x.append(ask_qps)
+                    new_y = answer_y(last_point[0], last_point[1], statistics.mean(raw_data[key]["actual"]),statistics.mean(raw_data[key]["cpu"]), find_qps )
+                    
+                    y.append(new_y*(new_pod_number / old_pod_number))
+                
+            last_point = [statistics.mean(raw_data[key]["actual"]), statistics.mean(raw_data[key]["cpu"])]
+
+    plt.plot(x,y,label=plot_label)
+
+    
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3 :
@@ -100,4 +165,7 @@ if __name__ == "__main__":
         # call function to calculate average and add datapoints
         plot_from_file(sys.argv[2:]) 
         data_process_and_visualize()
+        #for key in datas:
+        #    print(key)
+        #print(datas["nodejs: 1pod-200mCPU-64Mi"])
         #print_data()
--- run_more_times_measurement.sh
+++ run_more_times_measurement.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {1..5}
+for i in {1..5} #deadline is comming
 do
     echo "=== Start measurement #$i ==="
-    time python3 benchmark.py configs/hello-nodejs-measurement-config.yaml
+    time python3 benchmark.py configs/nginx-long-measurement-config-bugfix.yaml
     echo "=== End ==="
 done
