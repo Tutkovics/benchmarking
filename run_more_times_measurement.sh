6ea8d4334d7e0c2e10711ccf1bd07c9148b475d4
Tutkovics <tutiandras@gmail.com>
2019-12-08 15:34:45 +0100

    new measurments

--- run_more_times_measurement.sh
+++ run_more_times_measurement.sh
@@ -2,6 +2,6 @@
 for i in {1..5}
 do
     echo "=== Start measurement #$i ==="
-    time python3 benchmark.py configs/apache-measurement-config.yaml
+    time python3 benchmark.py configs/hello-nodejs-measurement-config.yaml
     echo "=== End ==="
 done
